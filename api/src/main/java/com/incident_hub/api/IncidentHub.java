88e01fa32f89bec6375f799911e575ab83c737d6
Aditya Verma <aditya.verma1768@gmail.com>
2025-01-20 13:33:43 +0530

    initial commit

---
+++ api/src/main/java/com/incident_hub/api/IncidentHub.java
@@ -0,0 +1,13 @@
+package com.incident_hub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentHub {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentHub.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/incident_hub/api/IncidentHubTests.java
@@ -0,0 +1,13 @@
+package com.incident_hub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentHubTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
